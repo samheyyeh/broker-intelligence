fdf682dc2be02beaefa881531c70faafbfb9ac38
Sam Yeh <samheyyeh@gmail.com>
2026-03-12 22:48:58 -0400

    fix crash on startup

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins="*", supports_credentials=False)
+CORS(app)
 
 ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
 PASSPHRASE = os.environ.get("PASSPHRASE", "")
